5e93aa8f7344249a54e78c1867b1170a6d1bf72c
Vladlen Fedosov <vladlen.f@namecheap.com>
2016-05-27 14:57:55 +0300

    Code refactoring

--- dist/acl.js
+++ dist/acl.js
@@ -1,4 +1,130 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Acl = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+/**
+ * @name AclRegistry
+ * @constructs AclRegistry
+ * @description Initializes a new ACL role registry
+ */
+function AclRegistry() {
+    var self = this;
+    var _storage = {};
+
+    self.add = function (item, parents /*null*/) {
+        parents = typeof parents === 'undefined' ? null : parents;
+
+        if (self.has(item)) {
+            throw new Error('Item "' + item + '" already exists in the registry');
+        }
+
+        var itemParents = [];
+
+        if (parents !== null) {
+            if (!Array.isArray(parents)) {
+                parents = [parents];
+            }
+
+            parents.forEach(function (parent) {
+                if (!self.has(parent)) {
+                    throw new Error('Parent role "' + parent + '" for "' + item + '" not exists in the registry');
+                }
+
+                itemParents.push(parent);
+                _storage[parent].children.push(item);
+            });
+        }
+
+        _storage[item] = {
+            id: item,
+            parents: itemParents,
+            children: []
+        };
+
+        return self;
+    };
+
+    self.get = function (item) {
+        if (!self.has(item)) {
+            throw new Error('Item "' + item + '" not exists in the registry');
+        }
+
+        return item;
+    };
+
+    self.has = function (item) {
+        return _storage[item] !== undefined;
+    };
+
+    self.getParents = function (item) {
+        if (!self.has(item)) {
+            throw new Error('Item "' + item + '" not exists in the registry');
+        }
+
+        return _storage[item].parents;
+    };
+
+    self.inherits = function (item, inherit, onlyParents /*false*/) {
+        onlyParents = typeof onlyParents === 'undefined' ? false : onlyParents;
+
+        if (!self.has(item) || !self.has(inherit)) {
+            throw new Error('Items not exists in the registry');
+        }
+
+        var inherits = _storage[item].parents.indexOf(inherit) !== -1;
+
+        if (inherits || onlyParents) {
+            return inherits;
+        }
+
+        for (var i = 0; i < _storage[item].parents.length; i++) {
+            var parent = _storage[item].parents[i];
+
+            if (self.inherits(parent, inherit)) {
+                return true;
+            }
+        }
+
+        return false;
+    };
+
+    self.remove = function (item) {
+        if (!self.has(item)) {
+            throw new Error('Item "' + item + '" not exists in the registry');
+        }
+
+        _storage[item].children.forEach(function (child) {
+            var index = _storage[child].parents.indexOf(item);
+            if (index !== -1) {
+                _storage[child].parents.splice(index, 1);
+            }
+        });
+        _storage[item].parents.forEach(function (parent) {
+            var index = _storage[parent].children.indexOf(item);
+            if (index !== -1) {
+                _storage[parent].children.splice(index, 1);
+            }
+        });
+
+        delete _storage[item];
+
+        return self;
+    };
+
+    self.removeAll = function () {
+        _storage = {};
+
+        return self;
+    };
+
+    self.getItems = function () {
+        return JSON.parse(JSON.stringify(_storage));
+    };
+
+    return self;
+}
+
+module.exports = AclRegistry;
+},{}],"/src/index.js":[function(require,module,exports){
 /**
  * @interface AclResourceInterface
  */
@@ -24,7 +150,7 @@
  */
 
 
-var AclRegistry = require('./AclRegistry');
+var AclRegistry = require('./acl-registry');
 
 function Acl() {
     var self = this;
@@ -1115,137 +1241,5 @@ function Acl() {
 }
 
 module.exports = Acl;
-},{"./AclRegistry":2}],2:[function(require,module,exports){
-'use strict';
-
-/**
- * @name AclRegistry
- * @constructs AclRegistry
- * @description Initializes a new ACL role registry
- */
-function AclRegistry() {
-    var self = this;
-    var _storage = {};
-
-    self.add = function (item, parents /*null*/) {
-        parents = typeof parents === 'undefined' ? null : parents;
-
-        if (self.has(item)) {
-            throw new Error('Item "' + item + '" already exists in the registry');
-        }
-
-        var itemParents = [];
-
-        if (parents !== null) {
-            if (!Array.isArray(parents)) {
-                parents = [parents];
-            }
-
-            parents.forEach(function (parent) {
-                if (!self.has(parent)) {
-                    throw new Error('Parent role "' + parent + '" for "' + item + '" not exists in the registry');
-                }
-
-                itemParents.push(parent);
-                _storage[parent].children.push(item);
-            });
-        }
-
-        _storage[item] = {
-            id: item,
-            parents: itemParents,
-            children: []
-        };
-
-        return self;
-    };
-
-    self.get = function (item) {
-        if (!self.has(item)) {
-            throw new Error('Item "' + item + '" not exists in the registry');
-        }
-
-        return item;
-    };
-
-    self.has = function (item) {
-        return _storage[item] !== undefined;
-    };
-
-    self.getParents = function (item) {
-        if (!self.has(item)) {
-            throw new Error('Item "' + item + '" not exists in the registry');
-        }
-
-        return _storage[item].parents;
-    };
-
-    self.inherits = function (item, inherit, onlyParents /*false*/) {
-        onlyParents = typeof onlyParents === 'undefined' ? false : onlyParents;
-
-        if (!self.has(item) || !self.has(inherit)) {
-            throw new Error('Items not exists in the registry');
-        }
-
-        var inherits = _storage[item].parents.indexOf(inherit) !== -1;
-
-        if (inherits || onlyParents) {
-            return inherits;
-        }
-
-        for (var i = 0; i < _storage[item].parents.length; i++) {
-            var parent = _storage[item].parents[i];
-
-            if (self.inherits(parent, inherit)) {
-                return true;
-            }
-        }
-
-        return false;
-    };
-
-    self.remove = function (item) {
-        if (!self.has(item)) {
-            throw new Error('Item "' + item + '" not exists in the registry');
-        }
-
-        _storage[item].children.forEach(function (child) {
-            var index = _storage[child].parents.indexOf(item);
-            if (index !== -1) {
-                _storage[child].parents.splice(index, 1);
-            }
-        });
-        _storage[item].parents.forEach(function (parent) {
-            var index = _storage[parent].children.indexOf(item);
-            if (index !== -1) {
-                _storage[parent].children.splice(index, 1);
-            }
-        });
-
-        delete _storage[item];
-
-        return self;
-    };
-
-    self.removeAll = function () {
-        _storage = {};
-
-        return self;
-    };
-
-    self.getItems = function () {
-        return JSON.parse(JSON.stringify(_storage));
-    };
-
-    return self;
-}
-
-module.exports = AclRegistry;
-},{}],"/src/index.js":[function(require,module,exports){
-'use strict';
-
-var Acl = require('./Acl');
-
-module.exports = Acl;
-},{"./Acl":1}]},{},[])("/src/index.js")
+},{"./acl-registry":1}]},{},[])("/src/index.js")
 });
